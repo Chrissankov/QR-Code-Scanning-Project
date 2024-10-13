5109704f27ce7509a0e0394554bd567b5f77588d
Christian Tannoury <tsmchris42@gmail.com>
2024-10-14 00:04:39 +0300

    QR Code Component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <img src="../images/image-qr-code.png" alt="QR Code" />
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>
+        Scan the QR code to visit the Frontend Mentor and take your coding
+        skills to the next level
+      </p>
+    </div>
+  );
+}
